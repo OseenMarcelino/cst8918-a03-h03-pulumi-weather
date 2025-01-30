ffb42e67da41e1a4f367ba3d7cc7e8e4d3794857
OseenM <oseenmarcelino.6@gmail.com>
2025-01-29 23:43:29 -0500

    Update the IaC Definition

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,3 +1,4 @@
+import * as cache from '@pulumi/azure-native/cache'
 import * as resources from '@pulumi/azure-native/resources'
 import * as containerregistry from '@pulumi/azure-native/containerregistry'
 import * as pulumi from "@pulumi/pulumi";
@@ -18,6 +19,30 @@ const cpu = config.requireNumber('cpu')
 const memory = config.requireNumber('memory')
 // Create a resource group.
 const resourceGroup = new resources.ResourceGroup(`${prefixName}rg`)
+// Create a managed Redis service
+const redis = new cache.Redis(`${prefixName}-redis`, {
+  name: `${prefixName}-weather-cache`,
+  location: 'westus3',
+  resourceGroupName: resourceGroup.name,
+  enableNonSslPort: true,
+  redisVersion: 'Latest',
+  minimumTlsVersion: '1.2',
+  redisConfiguration: {
+    maxmemoryPolicy: 'allkeys-lru'
+  },
+  sku: {
+    name: 'Basic',
+    family: 'C',
+    capacity: 0
+  }
+})
+// Extract the auth creds from the deployed Redis service
+const redisAccessKey = cache
+  .listRedisKeysOutput({ name: redis.name, resourceGroupName: resourceGroup.name })
+  .apply(keys => keys.primaryKey)
+
+// Construct the Redis connection string to be passed as an environment variable in the app container
+const redisConnectionString = pulumi.interpolate`rediss://:${redisAccessKey}@${redis.hostName}:${redis.sslPort}`
 
 // Create the container registry.
 const registry = new containerregistry.Registry(`${prefixName}ACR`, {
@@ -85,6 +110,10 @@ const containerGroup = new containerinstance.ContainerGroup(
             name: 'WEATHER_API_KEY',
             value: config.requireSecret('weatherApiKey')
           },
+          {
+            name: 'REDIS_URL',
+            value: redisConnectionString
+          }
         ],
         resources: {
           requests: {
@@ -106,6 +135,8 @@ const containerGroup = new containerinstance.ContainerGroup(
     },
   },
 )
+
+
 // Export the service's IP address, hostname, and fully-qualified URL.
 export const hostname = containerGroup.ipAddress.apply((addr) => addr!.fqdn!)
 export const ip = containerGroup.ipAddress.apply((addr) => addr!.ip!)
